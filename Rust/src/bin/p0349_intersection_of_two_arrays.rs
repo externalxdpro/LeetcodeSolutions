08cf18d974eb94b2df35f53f3f9000dc8cb7c31c
externalxdpro <externalxdpro@gmail.com>
2024-03-09 20:06:08 -0500

    Add daily problem

---
+++ Rust/src/bin/p0349_intersection_of_two_arrays.rs
@@ -0,0 +1,59 @@
+/**
+ * [349] Intersection of Two Arrays
+ *
+ * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+ *
+ * Example 1:
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ *
+ * Example 2:
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * Explanation: [4,9] is also accepted.
+ *
+ * Constraints:
+ *     1 <= nums1.length, nums2.length <= 1000
+ *     0 <= nums1[i], nums2[i] <= 1000
+ *
+ */
+
+pub struct Solution {}
+use std::collections::HashSet;
+
+// problem: https://leetcode.com/problems/intersection-of-two-arrays/
+// discuss: https://leetcode.com/problems/intersection-of-two-arrays/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let nums1_set: HashSet<i32> = nums1.into_iter().collect();
+        let mut result: HashSet<i32> = HashSet::new();
+
+        for i in nums2 {
+            if nums1_set.contains(&i) {
+                result.insert(i);
+            }
+        }
+
+        result.into_iter().collect()
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_349() {
+        assert_eq!(
+            Solution::intersection(vec![1, 2, 2, 1], vec![2, 2]),
+            vec![2]
+        )
+    }
+}
+
+fn main() {}
