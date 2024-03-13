dfdf0073041cbaec7c0962c22da748c984271813
externalxdpro <externalxdpro@gmail.com>
2024-03-13 00:03:49 -0400

    Add daily problem

---
+++ Rust/src/bin/p2485_find_the_pivot_integer.rs
@@ -0,0 +1,62 @@
+/**
+* [2485] Find the Pivot Integer
+*
+* Given a positive integer n, find the pivot integer x such that:
+*     The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
+* Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
+*
+* Example 1:
+* Input: n = 8
+* Output: 6
+* Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
+*
+* Example 2:
+* Input: n = 1
+* Output: 1
+* Explanation: 1 is the pivot integer since: 1 = 1.
+*
+* Example 3:
+* Input: n = 4
+* Output: -1
+* Explanation: It can be proved that no such integer exist.
+*
+* Constraints:
+*     1 <= n <= 1000
+*
+*/
+
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn pivot_integer(n: i32) -> i32 {
+        let mut l_sum = 0;
+        let mut r_sum = n * (n + 1) / 2;
+
+        for i in 1..=n {
+            l_sum += i;
+            if l_sum == r_sum {
+                return i;
+            }
+            r_sum -= i;
+        }
+        -1
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_2485() {
+        assert_eq!(Solution::pivot_integer(8), 6);
+        assert_eq!(Solution::pivot_integer(1), 1);
+        assert_eq!(Solution::pivot_integer(4), -1);
+    }
+}
+
+fn main() {}
