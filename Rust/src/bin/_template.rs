bc764b71f1b4e4de6018e5e916f54457fb39bd32
externalxdpro <externalxdpro@gmail.com>
2024-03-17 09:31:01 -0400

    Add template for rust

---
+++ Rust/src/bin/_template.rs
@@ -0,0 +1,24 @@
+// [#] Name
+// Difficulty:
+
+// Description
+
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn test_num() {
+        // assert_eq!(Solution::funcname(), ());
+    }
+}
+
+fn main() {}
