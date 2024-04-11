e97325c4e44694f22f849036aba90b776d3b2beb
externalxdpro <externalxdpro@gmail.com>
2024-04-10 22:51:05 -0400

    Add shorter solution

--- Rust/src/bin/p0402_remove_k_digits.rs
+++ Rust/src/bin/p0402_remove_k_digits.rs
@@ -28,36 +28,27 @@ pub struct Solution {}
 // submission codes start here
 
 impl Solution {
-    pub fn remove_kdigits(num: String, mut k: i32) -> String {
-        if num.len() as i32 == k {
-            return "0".to_string();
-        }
-
+    pub fn remove_kdigits(num: String, k: i32) -> String {
+        let mut k = k as usize;
         let mut stack: Vec<char> = Vec::new();
-        for i in 0..num.len() {
-            while k > 0 && !stack.is_empty() && *stack.last().unwrap() > num.as_bytes()[i] as char {
+
+        for i in num.chars() {
+            while k > 0 && !stack.is_empty() && *stack.last().unwrap() > i {
                 stack.pop();
                 k -= 1;
             }
-            stack.push(num.as_bytes()[i] as char);
+            stack.push(i);
         }
-        while k > 0 {
+        for _ in 0..k {
             stack.pop();
-            k -= 1;
         }
 
-        let mut result = String::new();
-        for i in stack {
-            if i == '0' && result.is_empty() {
-                continue;
-            }
-            result.push(i);
-        }
-
+        let result = stack.into_iter().collect::<String>();
+        let result = result.trim_start_matches('0');
         if result.is_empty() {
             "0".to_string()
         } else {
-            result
+            result.to_string()
         }
     }
 }
