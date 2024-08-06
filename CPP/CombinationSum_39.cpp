4e7fc50d5386e0dc3ccbab1c02ebe956d4a2019b
externalxdpro <externalxdpro@gmail.com>
2024-08-06 02:47:48 -0400

    Add p39

---
+++ CPP/CombinationSum_39.cpp
@@ -0,0 +1,80 @@
+// 39. Combination Sum
+// Difficulty: Medium
+
+// Given an array of distinct integers candidates and a target integer target,
+// return a list of all unique combinations of candidates where the chosen
+// numbers sum to target. You may return the combinations in any order.
+// The same number may be chosen from candidates an unlimited number of times.
+// Two combinations are unique if the frequency of at least one of the chosen
+// numbers is different.
+// The test cases are generated such that the number of unique combinations that
+// sum up to target is less than 150 combinations for the given input.
+
+// Example 1:
+// Input: candidates = [2,3,6,7], target = 7
+// Output: [[2,2,3],[7]]
+// Explanation:
+// 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple
+// times. 7 is a candidate, and 7 = 7. These are the only two combinations.
+
+// Example 2:
+// Input: candidates = [2,3,5], target = 8
+// Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+// Example 3:
+// Input: candidates = [2], target = 1
+// Output: []
+
+// Constraints:
+//     1 <= candidates.length <= 30
+//     2 <= candidates[i] <= 40
+//     All elements of candidates are distinct.
+//     1 <= target <= 40
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <vector>
+
+class Solution {
+  public:
+    std::vector<std::vector<int>> combinationSum(std::vector<int> &candidates,
+                                                 int               target) {
+        std::vector<std::vector<int>> result;
+        recurse(candidates, target, 0, 0, {}, result);
+        return result;
+    }
+
+  private:
+    void recurse(std::vector<int> &candidates, int target, int i, int sum,
+                 std::vector<int> curr, std::vector<std::vector<int>> &result) {
+        if (sum > target || i == candidates.size()) {
+            return;
+        } else if (sum == target) {
+            result.push_back(curr);
+            return;
+        }
+
+        recurse(candidates, target, i + 1, sum, curr, result);
+        sum += candidates[i];
+        curr.push_back(candidates[i]);
+        recurse(candidates, target, i, sum, curr, result);
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<std::pair<std::pair<std::vector<int>, int>,
+                          std::vector<std::vector<int>>>>
+        tests = {
+            {{{2, 3, 6, 7}, 7}, {{7}, {2, 2, 3}}},
+            {{{2, 3, 5}, 8}, {{3, 5}, {2, 3, 3}, {2, 2, 2, 2}}},
+            {{{2}, 1}, {}},
+        };
+
+    Solution solution;
+    for (auto &[test, ans] : tests) {
+        auto result = solution.combinationSum(test.first, test.second);
+        fmt::println("{} {}: {}", result == ans ? "✅" : "❌", test, result);
+    }
+
+    return 0;
+}
