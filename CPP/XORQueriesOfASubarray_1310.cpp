3ea917a97af502aeaca96b1728b7130b5dd207ae
externalxdpro <externalxdpro@gmail.com>
2024-09-13 09:38:26 -0400

    Add daily problem

---
+++ CPP/XORQueriesOfASubarray_1310.cpp
@@ -0,0 +1,73 @@
+// 1310. XOR Queries of a Subarray
+// Difficulty: Medium
+
+// You are given an array arr of positive integers. You are also given the array
+// queries where queries[i] = [lefti, righti].
+// For each query i compute the XOR of elements from lefti to righti (that is,
+// arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr[righti] ).
+// Return an array answer where answer[i] is the answer to the ith query.
+
+// Example 1:
+// Input: arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+// Output: [2,7,14,8]
+// Explanation:
+// The binary representation of the elements in the array are:
+// 1 = 0001
+// 3 = 0011
+// 4 = 0100
+// 8 = 1000
+// The XOR values for queries are:
+// [0,1] = 1 xor 3 = 2
+// [1,2] = 3 xor 4 = 7
+// [0,3] = 1 xor 3 xor 4 xor 8 = 14
+// [3,3] = 8
+
+// Example 2:
+// Input: arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+// Output: [8,0,4,4]
+
+// Constraints:
+//     1 <= arr.length, queries.length <= 3 * 10^4
+//     1 <= arr[i] <= 10^9
+//     queries[i].length == 2
+//     0 <= lefti <= righti < arr.length
+
+#include <fmt/ranges.h>
+#include <vector>
+
+class Solution {
+  public:
+    std::vector<int> xorQueries(std::vector<int>              &arr,
+                                std::vector<std::vector<int>> &queries) {
+        std::vector<int> result;
+        result.reserve(queries.size());
+
+        for (std::vector<int> &q : queries) {
+            int currXor = 0;
+            for (int i = q[0]; i <= q[1]; i++) {
+                currXor ^= arr[i];
+            }
+            result.push_back(currXor);
+        }
+
+        return result;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<
+        std::pair<std::pair<std::vector<int>, std::vector<std::vector<int>>>,
+                  std::vector<int>>>
+        tests = {
+            {{{1, 3, 4, 8}, {{0, 1}, {1, 2}, {0, 3}, {3, 3}}}, {2, 7, 14, 8}},
+            {{{4, 8, 2, 10}, {{2, 3}, {1, 3}, {0, 0}, {0, 3}}}, {8, 0, 4, 4}},
+        };
+
+    Solution solution;
+    for (auto &[test, ans] : tests) {
+        auto result = solution.xorQueries(test.first, test.second);
+        fmt::println("{} {}: {}", result == ans ? "✅" : "❌", test, result);
+    }
+
+    return 0;
+}
