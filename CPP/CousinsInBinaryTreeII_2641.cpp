b26b203984af177821852f6ff40c303be90a94ce
externalxdpro <externalxdpro@gmail.com>
2024-10-23 18:33:10 -0400

    Add daily problem

---
+++ CPP/CousinsInBinaryTreeII_2641.cpp
@@ -0,0 +1,91 @@
+// 2641. Cousins in Binary Tree II
+// Difficulty: Medium
+
+// Given the root of a binary tree, replace the value of each node in the tree
+// with the sum of all its cousins' values.
+
+// Two nodes of a binary tree are cousins if they have the same depth with
+// different parents.
+
+// Return the root of the modified tree.
+
+// Note that the depth of a node is the number of edges in the path from the
+// root node to it.
+
+// Example 1:
+
+// Input: root = [5,4,9,1,10,null,7]
+// Output: [0,0,0,7,7,null,11]
+// Explanation: The diagram above shows the initial binary tree and the binary
+// tree after changing the value of each node.
+// - Node with value 5 does not have any cousins so its sum is 0.
+// - Node with value 4 does not have any cousins so its sum is 0.
+// - Node with value 9 does not have any cousins so its sum is 0.
+// - Node with value 1 has a cousin with value 7 so its sum is 7.
+// - Node with value 10 has a cousin with value 7 so its sum is 7.
+// - Node with value 7 has cousins with values 1 and 10 so its sum is 11.
+
+// Example 2:
+
+// Input: root = [3,1,2]
+// Output: [0,0,0]
+// Explanation: The diagram above shows the initial binary tree and the binary
+// tree after changing the value of each node.
+// - Node with value 3 does not have any cousins so its sum is 0.
+// - Node with value 1 does not have any cousins so its sum is 0.
+// - Node with value 2 does not have any cousins so its sum is 0.
+
+// Constraints:
+
+//     The number of nodes in the tree is in the range [1, 105].
+//     1 <= Node.val <= 104
+
+#include <format>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+#include "_BinaryTree.hpp"
+
+class Solution {
+  public:
+    TreeNode *replaceValueInTree(TreeNode *root) {
+        std::queue<std::pair<TreeNode *, TreeNode *>> q{{{root, nullptr}}};
+
+        while (!q.empty()) {
+            std::vector<std::pair<TreeNode *, TreeNode *>> nodes;
+            int                                            sum = 0;
+            for (size_t n = q.size(); n > 0; n--) {
+                auto [l, r] = q.front();
+                q.pop();
+
+                if (l != nullptr &&
+                    (l->left != nullptr || l->right != nullptr)) {
+                    q.emplace(l->left, l->right);
+                }
+                if (r != nullptr &&
+                    (r->left != nullptr || r->right != nullptr)) {
+                    q.emplace(r->left, r->right);
+                }
+                nodes.emplace_back(l, r);
+                sum += getVal(l) + getVal(r);
+            }
+            for (auto &[l, r] : nodes) {
+                int curr = sum - getVal(l) - getVal(r);
+                if (l != nullptr) {
+                    l->val = curr;
+                }
+                if (r != nullptr) {
+                    r->val = curr;
+                }
+            }
+        }
+
+        return root;
+    }
+
+  private:
+    int getVal(TreeNode *node) { return !node ? 0 : node->val; }
+};
+
+int main(int argc, char *argv[]) {}
