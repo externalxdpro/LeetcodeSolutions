8c7d85d8b605a988987439f1f7f3c590f1d46bed
externalxdpro <externalxdpro@gmail.com>
2024-12-26 19:33:27 -0500

    Add daily problem

---
+++ Rust/src/bin/p1014_best_sightseeing_pair.rs
@@ -0,0 +1,50 @@
+// 1014. Best Sightseeing Pair
+// Difficulty: Medium
+
+// You are given an integer array values where values[i] represents the value of the ith sightseeing spot. Two sightseeing spots i and j have a distance j - i between them.
+
+// The score of a pair (i < j) of sightseeing spots is values[i] + values[j] + i - j: the sum of the values of the sightseeing spots, minus the distance between them.
+
+// Return the maximum score of a pair of sightseeing spots.
+
+// Example 1:
+
+// Input: values = [8,1,5,2,6]
+// Output: 11
+// Explanation: i = 0, j = 2, values[i] + values[j] + i - j = 8 + 5 + 0 - 2 = 11
+
+// Example 2:
+
+// Input: values = [1,2]
+// Output: 2
+
+// Constraints:
+
+//     2 <= values.length <= 5 * 104
+//     1 <= values[i] <= 1000
+
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn max_score_sightseeing_pair(values: Vec<i32>) -> i32 {
+        let mut result = 0;
+        for i in 0..values.len() {
+            for j in (i + 1)..values.len() {
+                result = result.max(values[i] + values[j] - (j - i) as i32);
+            }
+        }
+        result
+    }
+}
+
+// submission codes end
+
+fn main() {
+    assert_eq!(
+        Solution::max_score_sightseeing_pair(vec![8, 1, 5, 2, 6]),
+        11
+    );
+    assert_eq!(Solution::max_score_sightseeing_pair(vec![1, 2]), 2);
+}
