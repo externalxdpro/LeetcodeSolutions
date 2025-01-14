9e1be91ebf693150aa7193d1df189b366ec41504
externalxdpro <externalxdpro@gmail.com>
2025-01-14 00:38:51 -0500

    Add daily problem

---
+++ CPP/FindThePrefixCommonArrayOfTwoArrays_2657.cpp
@@ -0,0 +1,80 @@
+// 2657. Find the Prefix Common Array of Two Arrays
+// Difficulty: Medium
+
+// You are given two 0-indexed integer permutations A and B of length n.
+
+// A prefix common array of A and B is an array C such that C[i] is equal to the
+// count of numbers that are present at or before the index i in both A and B.
+
+// Return the prefix common array of A and B.
+
+// A sequence of n integers is called a permutation if it contains all integers
+// from 1 to n exactly once.
+
+// Example 1:
+
+// Input: A = [1,3,2,4], B = [3,1,2,4]
+// Output: [0,2,3,4]
+// Explanation: At i = 0: no number is common, so C[0] = 0.
+// At i = 1: 1 and 3 are common in A and B, so C[1] = 2.
+// At i = 2: 1, 2, and 3 are common in A and B, so C[2] = 3.
+// At i = 3: 1, 2, 3, and 4 are common in A and B, so C[3] = 4.
+
+// Example 2:
+
+// Input: A = [2,3,1], B = [3,1,2]
+// Output: [0,1,3]
+// Explanation: At i = 0: no number is common, so C[0] = 0.
+// At i = 1: only 3 is common in A and B, so C[1] = 1.
+// At i = 2: 1, 2, and 3 are common in A and B, so C[2] = 3.
+
+// Constraints:
+
+//     1 <= A.length == B.length == n <= 50
+//     1 <= A[i], B[i] <= n
+//     It is guaranteed that A and B are both a permutation of n integers.
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+  public:
+    std::vector<int> findThePrefixCommonArray(std::vector<int> &A,
+                                              std::vector<int> &B) {
+        int                     n = A.size();
+        std::unordered_set<int> a, b;
+        std::vector<int>        result(n);
+
+        for (int i = 0; i < n; i++) {
+            a.insert(A[i]);
+            b.insert(B[i]);
+            for (int num : a) {
+                if (b.contains(num)) {
+                    result[i]++;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<std::pair<std::pair<std::vector<int>, std::vector<int>>,
+                          std::vector<int>>>
+        tests = {
+            {{{1, 3, 2, 4}, {3, 1, 2, 4}}, {0, 2, 3, 4}},
+            {{{2, 3, 1}, {3, 1, 2}}, {0, 1, 3}},
+        };
+
+    Solution solution;
+    for (auto &[test, ans] : tests) {
+        auto result =
+            solution.findThePrefixCommonArray(test.first, test.second);
+        fmt::println("{} {}: {}", result == ans ? "✅" : "❌", test, result);
+    }
+
+    return 0;
+}
