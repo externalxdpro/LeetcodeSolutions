197fd186a52c5a67777bc3f224a6801403c05913
externalxdpro <externalxdpro@gmail.com>
2025-03-24 00:08:13 -0400

    Add TLE solution

---
+++ CPP/CountDaysWithoutMeetings_3169.cpp
@@ -0,0 +1,101 @@
+// 3169. Count Days Without Meetings
+// Difficulty: Medium
+
+// You are given a positive integer days representing the total number of days
+// an employee is available for work (starting from day 1). You are also given a
+// 2D array meetings of size n where, meetings[i] = [start_i, end_i] represents
+// the starting and ending days of meeting i (inclusive).
+
+// Return the count of days when the employee is available for work but no
+// meetings are scheduled.
+
+// Note: The meetings may overlap.
+
+// Example 1:
+
+// Input: days = 10, meetings = [[5,7],[1,3],[9,10]]
+
+// Output: 2
+
+// Explanation:
+
+// There is no meeting scheduled on the 4th and 8th days.
+
+// Example 2:
+
+// Input: days = 5, meetings = [[2,4],[1,3]]
+
+// Output: 1
+
+// Explanation:
+
+// There is no meeting scheduled on the 5th day.
+
+// Example 3:
+
+// Input: days = 6, meetings = [[1,6]]
+
+// Output: 0
+
+// Explanation:
+
+// Meetings are scheduled for all working days.
+
+// Constraints:
+
+//     1 <= days <= 109
+//     1 <= meetings.length <= 105
+//     meetings[i].length == 2
+//     1 <= meetings[i][0] <= meetings[i][1] <= days
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <vector>
+
+class Solution {
+  public:
+    int countDays(int days, std::vector<std::vector<int>> &meetings) {
+        std::ranges::sort(meetings);
+        for (int i = 0; i < meetings.size() - 1; i++) {
+            if (meetings[i][1] >= meetings[i + 1][0]) {
+                meetings[i][1] = std::max(meetings[i][1], meetings[i + 1][1]);
+                meetings.erase(meetings.begin() + i + 1);
+                i--;
+            }
+        }
+        int result = days;
+        for (auto &m : meetings) {
+            result -= m[1] - m[0] + 1;
+        }
+        return result;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<std::pair<std::pair<int, std::vector<std::vector<int>>>, int>>
+        tests = {
+            {{10, {{5, 7}, {1, 3}, {9, 10}}}, 2},
+            {{5, {{2, 4}, {1, 3}}}, 1},
+            {{6, {{1, 6}}}, 0},
+            {{14,
+              {{6, 11},
+               {7, 13},
+               {8, 9},
+               {5, 8},
+               {3, 13},
+               {11, 13},
+               {1, 3},
+               {5, 10},
+               {8, 13},
+               {3, 9}}},
+             1},
+        };
+
+    Solution solution;
+    for (auto &[test, ans] : tests) {
+        auto result = solution.countDays(test.first, test.second);
+        fmt::println("{} {}: {}", result == ans ? "✅" : "❌", test, result);
+    }
+
+    return 0;
+}
