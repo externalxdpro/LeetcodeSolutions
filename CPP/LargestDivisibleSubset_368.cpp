15b69f8f178d5e7909894f9cb52ab6eb58a70aeb
externalxdpro <externalxdpro@gmail.com>
2025-04-05 22:56:05 -0400

    Add Kahn's algorithm solution for daily problem

--- CPP/LargestDivisibleSubset_368.cpp
+++ CPP/LargestDivisibleSubset_368.cpp
@@ -22,102 +22,60 @@
 
 #include <algorithm>
 #include <fmt/ranges.h>
-#include <map>
+#include <queue>
 #include <vector>
 
-// Recursive w/ memo, too slow
-// class Solution {
-//   public:
-//     std::vector<int> largestDivisibleSubset(std::vector<int> &nums) {
-//         std::ranges::sort(nums);
-
-//         std::map<std::pair<int, int>, std::vector<int>> memo;
-//         std::vector<int> result = dp(nums, 0, -1, memo);
-
-//         fmt::print("{}\n", memo);
-//         return result;
-//     }
-
-//   private:
-//     std::vector<int> dp(std::vector<int> &nums, int i, int prev,
-//                         std::map<std::pair<int, int>, std::vector<int>>
-//                         &memo) {
-//         if (i >= nums.size()) {
-//             return {};
-//         }
-
-//         if (memo.contains({i, prev})) {
-//             return memo[{i, prev}];
-//         }
-
-//         std::vector<int> skip = dp(nums, i + 1, prev, memo);
-//         // skip.insert(skip.begin(), nums[i]);
-//         // skip.push_back(nums[i]);
-
-//         std::vector<int> take;
-//         if (prev == -1 || nums[i] % nums[prev] == 0) {
-//             take = dp(nums, i + 1, i, memo);
-//             // take.insert(take.begin(), nums[i]);
-//             take.push_back(nums[i]);
-//         }
-
-//         // if (skip.size() > take.size()) {
-//         //     memo[{i, prev}] = skip;
-//         // } else {
-//         //     memo[{i, prev}] = take;
-//         // }
-//         memo[{i, prev}] = std::max(skip, take);
-//         return memo[{i, prev}];
-//     }
-// };
-
-// Bottom up
 class Solution {
   public:
     std::vector<int> largestDivisibleSubset(std::vector<int> &nums) {
-        if (nums.size() == 1) {
-            return nums;
-        }
-
         std::ranges::sort(nums);
-        std::vector<std::vector<int>> memo(nums.size());
-        memo.back() = {nums.back()};
+        std::vector<int>              deg(nums.size());
+        std::vector<int>              prev(nums.size(), -1);
+        std::vector<std::vector<int>> adj(nums.size());
 
-        int size  = 0;
-        int index = -1;
-        for (int i = nums.size() - 2; i >= 0; i--) {
-            int tempSize  = 0;
-            int tempIndex = -1;
+        for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[j] % nums[i] == 0 && memo[j].size() > tempSize) {
-                    tempSize  = memo[j].size();
-                    tempIndex = j;
+                if (nums[j] % nums[i] == 0) {
+                    adj[i].push_back(j);
+                    deg[j]++;
                 }
             }
+        }
 
-            memo[i] = {nums[i]};
-
-            if (tempIndex != -1) {
-                memo[i].insert(memo[i].end(), memo[tempIndex].begin(),
-                               memo[tempIndex].end());
-            }
-
-            if (size < memo[i].size()) {
-                size  = memo[i].size();
-                index = i;
+        std::queue<int> q;
+        for (int i = 0; i < nums.size(); i++) {
+            if (deg[i] == 0) {
+                q.push(i);
             }
         }
 
-        return memo[index];
+        int last = 0;
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for (int next : adj[curr]) {
+                if (--deg[next] == 0) {
+                    q.push(next);
+                    prev[next] = curr;
+                }
+            }
+            last = curr;
+        }
+
+        std::vector<int> result;
+        for (int i = last; i != -1; i = prev[i]) {
+            result.push_back(nums[i]);
+        }
+        return result;
     }
 };
 
 int main(int argc, char *argv[]) {
     std::vector<std::pair<std::vector<int>, std::vector<int>>> tests = {
-        {{1, 2, 3}, {1, 2}},
-        {{1, 2, 4, 8}, {1, 2, 4, 8}},
+        {{1, 2, 3}, {3, 1}},
+        {{1, 2, 4, 8}, {8, 4, 2, 1}},
         {{1}, {1}},
-        {{3, 17}, {3}},
+        {{3, 17}, {17}},
     };
 
     Solution solution;
