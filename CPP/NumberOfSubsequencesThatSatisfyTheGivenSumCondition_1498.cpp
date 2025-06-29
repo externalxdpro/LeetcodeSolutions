6376ee74660ec8e141d038c1c8013409c09c71af
externalxdpro <externalxdpro@gmail.com>
2025-06-28 21:35:03 -0400

    Add daily problem

---
+++ CPP/NumberOfSubsequencesThatSatisfyTheGivenSumCondition_1498.cpp
@@ -0,0 +1,93 @@
+// 1498. Number of Subsequences That Satisfy the Given Sum Condition
+// Difficulty: Medium
+
+// You are given an array of integers nums and an integer target.
+
+// Return the number of non-empty subsequences of nums such that the sum of the
+// minimum and maximum element on it is less or equal to target. Since the
+// answer may be too large, return it modulo 109 + 7.
+
+// Example 1:
+
+// Input: nums = [3,5,6,7], target = 9
+// Output: 4
+// Explanation: There are 4 subsequences that satisfy the condition.
+// [3] -> Min value + max value <= target (3 + 3 <= 9)
+// [3,5] -> (3 + 5 <= 9)
+// [3,5,6] -> (3 + 6 <= 9)
+// [3,6] -> (3 + 6 <= 9)
+
+// Example 2:
+
+// Input: nums = [3,3,6,8], target = 10
+// Output: 6
+// Explanation: There are 6 subsequences that satisfy the condition. (nums can
+// have repeated numbers). [3] , [3] , [3,3], [3,6] , [3,6] , [3,3,6]
+
+// Example 3:
+
+// Input: nums = [2,3,3,4,6,7], target = 12
+// Output: 61
+// Explanation: There are 63 non-empty subsequences, two of them do not satisfy
+// the condition ([6,7], [7]). Number of valid subsequences (63 - 2 = 61).
+
+// Constraints:
+
+//     1 <= nums.length <= 105
+//     1 <= nums[i] <= 106
+//     1 <= target <= 106
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <vector>
+
+class Solution {
+  public:
+    int numSubseq(std::vector<int> &nums, int target) {
+        const int MOD = 1e9 + 7;
+        std::ranges::sort(nums);
+        std::vector<int> pow(nums.size(), 1);
+        for (int i = 1; i < nums.size(); i++) {
+            pow[i] = pow[i - 1] * 2 % MOD;
+        }
+
+        int result = 0;
+        for (int l = 0; l < nums.size(); l++) {
+            int r = search(nums, target - nums[l]) - 1;
+            if (l <= r) {
+                result = (result + pow[r - l]) % MOD;
+            }
+        }
+        return result;
+    }
+
+  private:
+    int search(std::vector<int> &nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] <= target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return l;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<std::pair<std::pair<std::vector<int>, int>, int>> tests = {
+        {{{3, 5, 6, 7}, 9}, 4},
+        {{{3, 3, 6, 8}, 10}, 6},
+        {{{2, 3, 3, 4, 6, 7}, 12}, 61},
+    };
+
+    Solution solution;
+    for (auto &[test, ans] : tests) {
+        auto result = solution.numSubseq(test.first, test.second);
+        fmt::println("{} {}: {}", result == ans ? "✅" : "❌", test, result);
+    }
+
+    return 0;
+}
