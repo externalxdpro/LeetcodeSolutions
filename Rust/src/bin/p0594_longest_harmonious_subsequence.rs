12cfe0df8db408660ebfeaaf8cdeb2774b9ef7ff
externalxdpro <externalxdpro@gmail.com>
2025-06-29 20:33:55 -0400

    Add daily problem

---
+++ Rust/src/bin/p0594_longest_harmonious_subsequence.rs
@@ -0,0 +1,74 @@
+// 594. Longest Harmonious Subsequence
+// Difficulty: Easy
+
+// We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+
+// Given an integer array nums, return the length of its longest harmonious
+
+// among all its possible subsequences.
+
+// Example 1:
+
+// Input: nums = [1,3,2,2,5,2,3,7]
+
+// Output: 5
+
+// Explanation:
+
+// The longest harmonious subsequence is [3,2,2,2,3].
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+
+// Output: 2
+
+// Explanation:
+
+// The longest harmonious subsequences are [1,2], [2,3], and [3,4], all of which have a length of 2.
+
+// Example 3:
+
+// Input: nums = [1,1,1,1]
+
+// Output: 0
+
+// Explanation:
+
+// No harmonic subsequence exists.
+
+// Constraints:
+
+//     1 <= nums.length <= 2 * 104
+//     -109 <= nums[i] <= 109
+
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn find_lhs(nums: Vec<i32>) -> i32 {
+        use std::collections::HashMap;
+        let freq: HashMap<i32, i32> = nums.into_iter().fold(HashMap::new(), |mut acc, x| {
+            acc.entry(x).and_modify(|v| *v += 1).or_insert(1);
+            acc
+        });
+
+        let mut result = 0;
+        for (i, v) in freq.iter() {
+            if let Some(&x) = freq.get(&(i + 1)) {
+                result = std::cmp::max(result, v + x);
+            }
+        }
+
+        result
+    }
+}
+
+// submission codes end
+
+fn main() {
+    assert_eq!(Solution::find_lhs(vec![1, 3, 2, 2, 5, 2, 3, 7]), 5);
+    assert_eq!(Solution::find_lhs(vec![1, 2, 3, 4]), 2);
+    assert_eq!(Solution::find_lhs(vec![1, 1, 1, 1]), 0);
+}
