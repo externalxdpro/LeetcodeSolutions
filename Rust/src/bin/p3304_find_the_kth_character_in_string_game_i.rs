ab1dcc71ac6f5279bebf08575214b389bb3799ec
externalxdpro <externalxdpro@gmail.com>
2025-07-02 21:33:48 -0400

    Add daily problem

---
+++ Rust/src/bin/p3304_find_the_kth_character_in_string_game_i.rs
@@ -0,0 +1,71 @@
+// 3304. Find the K-th Character in String Game I
+// Difficulty: Easy
+
+// Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+
+// You are given a positive integer k.
+
+// Now Bob will ask Alice to perform the following operation forever:
+
+//     Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word.
+
+// For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+
+// Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+
+// Note that the character 'z' can be changed to 'a' in the operation.
+
+// Example 1:
+
+// Input: k = 5
+
+// Output: "b"
+
+// Explanation:
+
+// Initially, word = "a". We need to do the operation three times:
+
+//     Generated string is "b", word becomes "ab".
+//     Generated string is "bc", word becomes "abbc".
+//     Generated string is "bccd", word becomes "abbcbccd".
+
+// Example 2:
+
+// Input: k = 10
+
+// Output: "c"
+
+// Constraints:
+
+//     1 <= k <= 500
+
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn kth_character(k: i32) -> char {
+        let k = k as usize;
+        let mut word = vec!['a'];
+
+        while word.len() < k {
+            word.extend(word.clone().iter().map(|&c| Self::inc(c)));
+        }
+
+        word[k - 1]
+    }
+
+    fn inc(c: char) -> char {
+        match c {
+            'z' => 'a',
+            _ => (c as u8 + 1) as char,
+        }
+    }
+}
+
+// submission codes end
+
+fn main() {
+    assert_eq!(Solution::kth_character(5), 'b');
+    assert_eq!(Solution::kth_character(10), 'c');
+}
