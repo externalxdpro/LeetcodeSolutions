ea507fb30dfa9948d110b0624920b4ec21746cf3
externalxdpro <externalxdpro@gmail.com>
2025-07-11 00:15:09 -0400

    Add daily problem

---
+++ Rust/src/bin/p2402_meeting_rooms_iii.rs
@@ -0,0 +1,156 @@
+// 2402. Meeting Rooms III
+// Difficulty: Hard
+
+// You are given an integer n. There are n rooms numbered from 0 to n - 1.
+
+// You are given a 2D integer array meetings where meetings[i] = [starti, endi] means that a meeting will be held during the half-closed time interval [starti, endi). All the values of starti are unique.
+
+// Meetings are allocated to rooms in the following manner:
+
+//     Each meeting will take place in the unused room with the lowest number.
+//     If there are no available rooms, the meeting will be delayed until a room becomes free. The delayed meeting should have the same duration as the original meeting.
+//     When a room becomes unused, meetings that have an earlier original start time should be given the room.
+
+// Return the number of the room that held the most meetings. If there are multiple rooms, return the room with the lowest number.
+
+// A half-closed interval [a, b) is the interval between a and b including a and not including b.
+
+// Example 1:
+
+// Input: n = 2, meetings = [[0,10],[1,5],[2,7],[3,4]]
+// Output: 0
+// Explanation:
+// - At time 0, both rooms are not being used. The first meeting starts in room 0.
+// - At time 1, only room 1 is not being used. The second meeting starts in room 1.
+// - At time 2, both rooms are being used. The third meeting is delayed.
+// - At time 3, both rooms are being used. The fourth meeting is delayed.
+// - At time 5, the meeting in room 1 finishes. The third meeting starts in room 1 for the time period [5,10).
+// - At time 10, the meetings in both rooms finish. The fourth meeting starts in room 0 for the time period [10,11).
+// Both rooms 0 and 1 held 2 meetings, so we return 0.
+
+// Example 2:
+
+// Input: n = 3, meetings = [[1,20],[2,10],[3,5],[4,9],[6,8]]
+// Output: 1
+// Explanation:
+// - At time 1, all three rooms are not being used. The first meeting starts in room 0.
+// - At time 2, rooms 1 and 2 are not being used. The second meeting starts in room 1.
+// - At time 3, only room 2 is not being used. The third meeting starts in room 2.
+// - At time 4, all three rooms are being used. The fourth meeting is delayed.
+// - At time 5, the meeting in room 2 finishes. The fourth meeting starts in room 2 for the time period [5,10).
+// - At time 6, all three rooms are being used. The fifth meeting is delayed.
+// - At time 10, the meetings in rooms 1 and 2 finish. The fifth meeting starts in room 1 for the time period [10,12).
+// Room 0 held 1 meeting while rooms 1 and 2 each held 2 meetings, so we return 1.
+
+// Constraints:
+
+//     1 <= n <= 100
+//     1 <= meetings.length <= 105
+//     meetings[i].length == 2
+//     0 <= starti < endi <= 5 * 105
+//     All the values of starti are unique.
+
+pub struct Solution {}
+
+// submission codes start here
+
+// impl Solution {
+//     pub fn most_booked(n: i32, mut meetings: Vec<Vec<i32>>) -> i32 {
+//         use std::{cmp::Reverse, collections::BinaryHeap};
+//         let n = n as usize;
+
+//         if n == 1 {
+//             return 0;
+//         }
+
+//         meetings.sort_unstable_by_key(|x| x[0]);
+
+//         let mut taken = vec![false; n];
+//         let mut counts = vec![0; n];
+//         let mut heap: BinaryHeap<Reverse<(i32, usize)>> = BinaryHeap::new();
+
+//         for i in 0..meetings.len() {
+//             while !heap.is_empty() && heap.peek().unwrap().0 .0 <= meetings[i][0] {
+//                 taken[heap.peek().unwrap().0 .1] = false;
+//                 heap.pop();
+//             }
+
+//             let j = taken.iter().position(|&x| x == false);
+//             if let Some(j) = j {
+//                 taken[j] = true;
+//                 counts[j] += 1;
+//                 heap.push(Reverse((meetings[i][1], j)));
+//             } else {
+//                 let top = heap.pop().unwrap().0;
+//                 counts[top.1] += 1;
+//                 heap.push(Reverse((meetings[i][1] + top.0 - meetings[i][0], top.1)));
+//             }
+//         }
+
+//         counts
+//             .into_iter()
+//             .enumerate()
+//             .rev()
+//             .max_by_key(|(_, x)| *x)
+//             .unwrap()
+//             .0 as i32
+//     }
+// }
+
+impl Solution {
+    pub fn most_booked(n: i32, mut meetings: Vec<Vec<i32>>) -> i32 {
+        use std::{cmp::Reverse, collections::BinaryHeap};
+        let n = n as usize;
+        meetings.sort_unstable_by_key(|x| x[0]);
+
+        let mut busy: BinaryHeap<Reverse<(u64, usize)>> = BinaryHeap::with_capacity(n);
+        let mut free: BinaryHeap<Reverse<usize>> = (0..n).map(|x| Reverse(x)).collect();
+        let mut counts = vec![0; n];
+
+        for m in meetings {
+            let (l, r) = (m[0] as u64, m[1] as u64);
+
+            while let Some(&Reverse((end, room))) = busy.peek() {
+                if end > l {
+                    break;
+                }
+                free.push(Reverse(room));
+                busy.pop();
+            }
+
+            let (new_end, room) = if !free.is_empty() {
+                (r, free.pop().unwrap().0)
+            } else {
+                let (end, room) = busy.pop().unwrap().0;
+                (end + r - l, room)
+            };
+
+            busy.push(Reverse((new_end, room)));
+            counts[room] += 1;
+        }
+
+        counts
+            .into_iter()
+            .enumerate()
+            .rev()
+            .max_by_key(|(_, x)| *x)
+            .unwrap()
+            .0 as i32
+    }
+}
+
+// submission codes end
+
+fn main() {
+    assert_eq!(
+        Solution::most_booked(2, vec![vec![0, 10], vec![1, 5], vec![2, 7], vec![3, 4]]),
+        0
+    );
+    assert_eq!(
+        Solution::most_booked(
+            3,
+            vec![vec![1, 20], vec![2, 10], vec![3, 5], vec![4, 9], vec![6, 8]]
+        ),
+        1
+    );
+}
