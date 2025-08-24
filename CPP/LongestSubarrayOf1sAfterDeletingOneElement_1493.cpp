b7f93d0948748d15cc2f58a9ce2bac992cbeaab0
externalxdpro <externalxdpro@gmail.com>
2025-08-23 22:50:19 -0400

    Add faster solution

--- CPP/LongestSubarrayOf1sAfterDeletingOneElement_1493.cpp
+++ CPP/LongestSubarrayOf1sAfterDeletingOneElement_1493.cpp
@@ -36,52 +36,25 @@
 #include <vector>
 
 class Solution {
-    struct element {
-        bool isOne;
-        int val;
-    };
-
   public:
     int longestSubarray(std::vector<int> &nums) {
-        std::vector<element> sections;
-        int ones = 0, others = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 1) {
-                if (others != 0) {
-                    sections.emplace_back(false, others);
-                    others = 0;
-                }
-                ones++;
-            } else {
-                if (ones != 0) {
-                    sections.emplace_back(true, ones);
-                    ones = 0;
-                }
-                others++;
+        int l = 0;
+        int zeroes = 0, result = 0;
+
+        for (int r = 0; r < nums.size(); r++) {
+            if (nums[r] == 0) {
+                zeroes++;
             }
-        }
-        if (ones != 0) {
-            sections.emplace_back(true, ones);
-        }
-        if (others != 0) {
-            sections.emplace_back(false, others);
-        }
-        if (sections.size() == 1 && sections[0].isOne) {
-            return sections[0].val - 1;
+            while (zeroes > 1) {
+                if (nums[l] == 0) {
+                    zeroes--;
+                }
+                l++;
+            }
+            result = std::max(result, r - l);
         }
 
-        int max = 0;
-        for (int i = 1; i < sections.size() - 1; i++) {
-            if (!sections[i].isOne && sections[i].val == 1) {
-                max = std::max(max, sections[i - 1].val + sections[i + 1].val);
-            }
-        }
-        for (auto &[isOne, val] : sections) {
-            if (isOne) {
-                max = std::max(max, val);
-            }
-        }
-        return max;
+        return result;
     }
 };
 
