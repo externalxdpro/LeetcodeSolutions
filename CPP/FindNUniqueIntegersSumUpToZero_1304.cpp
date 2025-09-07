8f2f792ccb08cfd512a3d2d6291349340aca70a8
externalxdpro <externalxdpro@gmail.com>
2025-09-06 22:22:30 -0400

    Add daily problem

---
+++ CPP/FindNUniqueIntegersSumUpToZero_1304.cpp
@@ -0,0 +1,58 @@
+// 1304. Find N Unique Integers Sum up to Zero
+// Difficulty: Easy
+
+// Given an integer n, return any array containing n unique integers such that
+// they add up to 0.
+
+// Example 1:
+
+// Input: n = 5
+// Output: [-7,-1,1,3,4]
+// Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+// Example 2:
+
+// Input: n = 3
+// Output: [-1,0,1]
+// Example 3:
+
+// Input: n = 1
+// Output: [0]
+
+// Constraints:
+
+// 1 <= n <= 1000
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <vector>
+
+class Solution {
+  public:
+    std::vector<int> sumZero(int n) {
+        std::vector<int> result;
+        for (int i = 1; i <= n / 2; i++) {
+            result.push_back(-i);
+            result.push_back(i);
+        }
+        if (n % 2 == 1) {
+            result.push_back(0);
+        }
+        return result;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<std::pair<int, std::vector<int>>> tests = {
+        {5, {-1, 1, -2, 2, 0}},
+        {3, {-1, 1, 0}},
+        {1, {0}},
+    };
+
+    Solution solution;
+    for (auto &[test, ans] : tests) {
+        auto result = solution.sumZero(test);
+        fmt::println("{} {}: {}", result == ans ? "✅" : "❌", test, result);
+    }
+
+    return 0;
+}
