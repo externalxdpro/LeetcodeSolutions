68990568c0b14fd06515828633e00eb275de216e
externalxdpro <externalxdpro@gmail.com>
2025-09-22 00:00:43 -0400

    Add daily problem

---
+++ Rust/src/bin/p3005_count_elements_with_maximum_frequency.rs
@@ -0,0 +1,50 @@
+// 3005. Count Elements With Maximum Frequency
+// Difficulty: Easy
+
+// You are given an array nums consisting of positive integers.
+
+// Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+// The frequency of an element is the number of occurrences of that element in the array.
+
+// Example 1:
+
+// Input: nums = [1,2,2,3,1,4]
+// Output: 4
+// Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+// So the number of elements in the array with maximum frequency is 4.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4,5]
+// Output: 5
+// Explanation: All elements of the array have a frequency of 1 which is the maximum.
+// So the number of elements in the array with maximum frequency is 5.
+
+// Constraints:
+
+//     1 <= nums.length <= 100
+//     1 <= nums[i] <= 100
+
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn max_frequency_elements(nums: Vec<i32>) -> i32 {
+        use std::collections::HashMap;
+        let freq = nums.iter().fold(HashMap::new(), |mut acc, &x| {
+            acc.entry(x).and_modify(|x| *x += 1).or_insert(1);
+            acc
+        });
+        let max = freq.iter().map(|(_, v)| v).max().unwrap().clone();
+        nums.into_iter().filter(|&x| freq[&x] == max).count() as i32
+    }
+}
+
+// submission codes end
+
+fn main() {
+    assert_eq!(Solution::max_frequency_elements(vec![1, 2, 2, 3, 1, 4]), 4);
+    assert_eq!(Solution::max_frequency_elements(vec![1, 2, 3, 4, 5]), 5);
+}
