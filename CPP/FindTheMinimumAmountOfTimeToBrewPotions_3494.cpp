5171c26ae03bbd94eadebe4bdd8dfe0f1f84da1c
externalxdpro <externalxdpro@gmail.com>
2025-10-08 20:48:17 -0400

    Add daily problem

---
+++ CPP/FindTheMinimumAmountOfTimeToBrewPotions_3494.cpp
@@ -0,0 +1,103 @@
+// 3494. Find the Minimum Amount of Time to Brew Potions
+// Difficulty: Medium
+
+// You are given two integer arrays, skill and mana, of length n and m,
+// respectively.
+
+// In a laboratory, n wizards must brew m potions in order. Each potion has a
+// mana capacity mana[j] and must pass through all the wizards sequentially to
+// be brewed properly. The time taken by the ith wizard on the jth potion is
+// timeij = skill[i] * mana[j].
+
+// Since the brewing process is delicate, a potion must be passed to the next
+// wizard immediately after the current wizard completes their work. This means
+// the timing must be synchronized so that each wizard begins working on a
+// potion exactly when it arrives. ​
+
+// Return the minimum amount of time required for the potions to be brewed
+// properly.
+
+// Example 1:
+
+// Input: skill = [1,5,2,4], mana = [5,1,4,2]
+
+// Output: 110
+
+// Explanation:
+// Potion Number	Start time	Wizard 0 done by	Wizard 1 done by
+// Wizard 2 done by	Wizard 3 done by 0	0	5	30	40
+// 60 1	52	53	58	60	64 2	54	58	78	86
+// 102 3	86	88	98	102	110
+
+// As an example for why wizard 0 cannot start working on the 1st potion before
+// time t = 52, consider the case where the wizards started preparing the 1st
+// potion at time t = 50. At time t = 58, wizard 2 is done with the 1st potion,
+// but wizard 3 will still be working on the 0th potion till time t = 60.
+
+// Example 2:
+
+// Input: skill = [1,1,1], mana = [1,1,1]
+
+// Output: 5
+
+// Explanation:
+
+//     Preparation of the 0th potion begins at time t = 0, and is completed by
+//     time t = 3. Preparation of the 1st potion begins at time t = 1, and is
+//     completed by time t = 4. Preparation of the 2nd potion begins at time t =
+//     2, and is completed by time t = 5.
+
+// Example 3:
+
+// Input: skill = [1,2,3,4], mana = [1,2]
+
+// Output: 21
+
+// Constraints:
+
+//     n == skill.length
+//     m == mana.length
+//     1 <= n, m <= 5000
+//     1 <= mana[i], skill[i] <= 5000
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <vector>
+
+class Solution {
+  public:
+    long long minTime(std::vector<int> &skill, std::vector<int> &mana) {
+        int n = skill.size(), m = mana.size();
+        std::vector<long long> memo(n);
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                if (i > 0) {
+                    memo[i] = std::max(memo[i - 1], memo[i]);
+                }
+                memo[i] += skill[i] * mana[j];
+            }
+            for (int i = n - 1; i > 0; i--) {
+                memo[i - 1] = memo[i] - skill[i] * mana[j];
+            }
+        }
+        return memo.back();
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<
+        std::pair<std::pair<std::vector<int>, std::vector<int>>, long long>>
+        tests = {
+            {{{1, 5, 2, 4}, {5, 1, 4, 2}}, 110},
+            {{{1, 1, 1}, {1, 1, 1}}, 5},
+            {{{1, 2, 3, 4}, {1, 2}}, 21},
+        };
+
+    Solution solution;
+    for (auto &[test, ans] : tests) {
+        auto result = solution.minTime(test.first, test.second);
+        fmt::println("{} {}: {}", result == ans ? "✅" : "❌", test, result);
+    }
+
+    return 0;
+}
