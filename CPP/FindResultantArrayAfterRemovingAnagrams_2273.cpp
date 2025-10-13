1d75b931f925c0a769af8bcb3bc110b408cb624d
externalxdpro <externalxdpro@gmail.com>
2025-10-13 15:01:31 -0400

    Add daily problem

---
+++ CPP/FindResultantArrayAfterRemovingAnagrams_2273.cpp
@@ -0,0 +1,99 @@
+// 2273. Find Resultant Array After Removing Anagrams
+// Difficulty: Easy
+
+// You are given a 0-indexed string array words, where words[i] consists of
+// lowercase English letters.
+
+// In one operation, select any index i such that 0 < i < words.length and
+// words[i - 1] and words[i] are anagrams, and delete words[i] from words. Keep
+// performing this operation as long as you can select an index that satisfies
+// the conditions.
+
+// Return words after performing all operations. It can be shown that selecting
+// the indices for each operation in any arbitrary order will lead to the same
+// result.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a
+// different word or phrase using all the original letters exactly once. For
+// example, "dacb" is an anagram of "abdc".
+
+// Example 1:
+
+// Input: words = ["abba","baba","bbaa","cd","cd"]
+// Output: ["abba","cd"]
+// Explanation:
+// One of the ways we can obtain the resultant array is by using the following
+// operations:
+// - Since words[2] = "bbaa" and words[1] = "baba" are anagrams, we choose index
+// 2 and delete words[2].
+//   Now words = ["abba","baba","cd","cd"].
+// - Since words[1] = "baba" and words[0] = "abba" are anagrams, we choose index
+// 1 and delete words[1].
+//   Now words = ["abba","cd","cd"].
+// - Since words[2] = "cd" and words[1] = "cd" are anagrams, we choose index 2
+// and delete words[2].
+//   Now words = ["abba","cd"].
+// We can no longer perform any operations, so ["abba","cd"] is the final
+// answer.
+
+// Example 2:
+
+// Input: words = ["a","b","c","d","e"]
+// Output: ["a","b","c","d","e"]
+// Explanation:
+// No two adjacent strings in words are anagrams of each other, so no operations
+// are performed.
+
+// Constraints:
+
+//     1 <= words.length <= 100
+//     1 <= words[i].length <= 10
+//     words[i] consists of lowercase English letters.
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+  public:
+    std::vector<std::string> removeAnagrams(std::vector<std::string> &words) {
+        for (int i = 1; i < words.size(); i++) {
+            if (words[i].size() != words[i - 1].size()) {
+                continue;
+            }
+
+            auto curr = strToFreq(words[i]), prev = strToFreq(words[i - 1]);
+            if (curr == prev) {
+                words.erase(words.begin() + i);
+                i--;
+            }
+        }
+        return words;
+    }
+
+    std::unordered_map<char, int> strToFreq(std::string &s) {
+        std::unordered_map<char, int> result;
+        for (char c : s) {
+            result[c]++;
+        }
+        return result;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<std::pair<std::vector<std::string>, std::vector<std::string>>>
+        tests = {
+            {{"abba", "baba", "bbaa", "cd", "cd"}, {"abba", "cd"}},
+            {{"a", "b", "c", "d", "e"}, {"a", "b", "c", "d", "e"}},
+            {{"abbb", "aaab"}, {"abbb", "aaab"}},
+        };
+
+    Solution solution;
+    for (auto &[test, ans] : tests) {
+        auto result = solution.removeAnagrams(test);
+        fmt::println("{} {}: {}", result == ans ? "✅" : "❌", test, result);
+    }
+
+    return 0;
+}
