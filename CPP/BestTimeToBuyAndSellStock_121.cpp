b4ad8da0bad3cc5c94688706c55b1839113a2c55
externalxdpro <externalxdpro@gmail.com>
2026-06-06 20:07:33 -0400

    Add p121

---
+++ CPP/BestTimeToBuyAndSellStock_121.cpp
@@ -0,0 +1,63 @@
+// 121. Best Time to Buy and Sell Stock
+// Difficulty: Easy
+
+// You are given an array prices where prices[i] is the price of a given stock
+// on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock
+// and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you
+// cannot achieve any profit, return 0.
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit =
+// 6-1 = 5. Note that buying on day 2 and selling on day 1 is not allowed
+// because you must buy before you sell. Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+// Constraints:
+
+// 1 <= prices.length <= 105
+// 0 <= prices[i] <= 104
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <vector>
+
+class Solution {
+  public:
+    int maxProfit(std::vector<int> &prices) {
+        int result = 0;
+        int r = prices.size() - 1;
+        for (int i = prices.size() - 2; i >= 0; i--) {
+            if (prices[i] > prices[r]) {
+                r = i;
+            }
+            result = std::max(result, prices[r] - prices[i]);
+        }
+
+        return result;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    std::vector<std::pair<std::vector<int>, int>> tests = {
+        {{7, 1, 5, 3, 6, 4}, 5},
+        {{7, 6, 4, 3, 1}, 0},
+    };
+
+    for (auto &[test, ans] : tests) {
+        Solution solution;
+        auto result = solution.maxProfit(test);
+        fmt::println("{} {}: {}", result == ans ? "✅" : "❌", test, result);
+    }
+
+    return 0;
+}
