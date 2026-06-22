94074d41e0fead9e4714f4294d793f0fc08fa0a0
externalxdpro <externalxdpro@gmail.com>
2026-06-22 10:04:12 -0400

    Add p142

---
+++ CPP/LinkedListCycleII_142.cpp
@@ -0,0 +1,66 @@
+// 142. Linked List Cycle II
+// Difficulty: Medium
+
+// Given the head of a linked list, return the node where the cycle begins. If
+// there is no cycle, return null.
+
+// There is a cycle in a linked list if there is some node in the list that can
+// be reached again by continuously following the next pointer. Internally, pos
+// is used to denote the index of the node that tail's next pointer is connected
+// to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as
+// a parameter.
+
+// Do not modify the linked list.
+
+// Example 1:
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the
+// second node. Example 2:
+
+// Input: head = [1,2], pos = 0
+// Output: tail connects to node index 0
+// Explanation: There is a cycle in the linked list, where tail connects to the
+// first node. Example 3:
+
+// Input: head = [1], pos = -1
+// Output: no cycle
+// Explanation: There is no cycle in the linked list.
+
+// Constraints:
+
+// The number of the nodes in the list is in the range [0, 104].
+// -105 <= Node.val <= 105
+// pos is -1 or a valid index in the linked-list.
+
+// Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <unordered_set>
+#include <vector>
+
+#include "_LinkedList.hpp"
+
+class Solution {
+  public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+
+        std::unordered_set<ListNode *> visited;
+        ListNode *curr = head;
+        while (curr != nullptr) {
+            if (visited.contains(curr)) {
+                return curr;
+            }
+            visited.insert(curr);
+            curr = curr->next;
+        }
+        return nullptr;
+    }
+};
+
+int main(int argc, char *argv[]) {}
