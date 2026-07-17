b00f3f38b0f16d9d1d40544a9d76f5bbfb8571db
externalxdpro <externalxdpro@gmail.com>
2026-07-17 09:45:59 -0400

    Add p208

---
+++ CPP/ImplementTriePrefixTree_208.cpp
@@ -0,0 +1,92 @@
+// 208. Implement Trie (Prefix Tree)
+// Difficulty: Medium
+
+// A trie (pronounced as "try") or prefix tree is a tree data structure used to
+// efficiently store and retrieve keys in a dataset of strings. There are
+// various applications of this data structure, such as autocomplete and
+// spellchecker.
+
+// Implement the Trie class:
+
+// Trie() Initializes the trie object.
+// void insert(String word) Inserts the string word into the trie.
+// boolean search(String word) Returns true if the string word is in the trie
+// (i.e., was inserted before), and false otherwise. boolean startsWith(String
+// prefix) Returns true if there is a previously inserted string word that has
+// the prefix prefix, and false otherwise.
+
+// Example 1:
+
+// Input
+// ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+// [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+// Output
+// [null, null, true, false, true, null, true]
+
+// Explanation
+// Trie trie = new Trie();
+// trie.insert("apple");
+// trie.search("apple");   // return True
+// trie.search("app");     // return False
+// trie.startsWith("app"); // return True
+// trie.insert("app");
+// trie.search("app");     // return True
+
+// Constraints:
+
+// 1 <= word.length, prefix.length <= 2000
+// word and prefix consist only of lowercase English letters.
+// At most 3 * 104 calls in total will be made to insert, search, and
+// startsWith.
+
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <unordered_map>
+#include <vector>
+
+class Trie {
+    struct Node {
+        std::unordered_map<char, Node *> children;
+        bool end = false;
+    };
+
+  public:
+    Node *root;
+
+    Trie() : root(new Node()) {}
+
+    void insert(std::string word) {
+        Node *curr = root;
+        for (char c : word) {
+            if (!curr->children.contains(c)) {
+                curr->children[c] = new Node();
+            }
+            curr = curr->children[c];
+        }
+        curr->end = true;
+    }
+
+    bool search(std::string word) {
+        Node *curr = root;
+        for (char c : word) {
+            if (!curr->children.contains(c)) {
+                return false;
+            }
+            curr = curr->children[c];
+        }
+        return curr->end;
+    }
+
+    bool startsWith(std::string prefix) {
+        Node *curr = root;
+        for (char c : prefix) {
+            if (!curr->children.contains(c)) {
+                return false;
+            }
+            curr = curr->children[c];
+        }
+        return true;
+    }
+};
+
+int main(int argc, char *argv[]) {}
